a4134f8dcf4551d110b9dc9c862def3d1e92514e
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-26 13:46:00 -0400

    fix: Standardize formatting of STAGE_NAME in OpenAPI Lambda function

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -124,7 +124,7 @@ resource "aws_lambda_function" "openapi" {
       REST_API_ID = aws_api_gateway_rest_api.incident_cmd.id
       # TODO: Remember to set the stage name when it changes
       # STAGE_NAME  = aws_api_gateway_stage.v1.stage_name
-      STAGE_NAME  = "v1"
+      STAGE_NAME = "v1"
     }
   }
 }
