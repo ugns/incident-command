5409e1992885bcacaf520482951ad7f957cf2713
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-26 05:24:22 -0400

    fix: Add wildcard media type to binary media types in API Gateway configuration

--- terraform/api_gateway_base.tf
+++ terraform/api_gateway_base.tf
@@ -5,7 +5,7 @@ resource "aws_api_gateway_rest_api" "incident_cmd" {
   endpoint_configuration {
     types = ["REGIONAL"]
   }
-  binary_media_types = ["application/pdf"]
+  binary_media_types = ["application/pdf", "*/*"]
 }
 
 data "aws_api_gateway_resource" "root" {
