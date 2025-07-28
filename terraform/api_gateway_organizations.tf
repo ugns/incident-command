1c7d20b92a0c1b198afc9189c4e3509b0231cbde
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-28 03:06:25 -0400

    refactor: Simplify API Gateway resource and method definitions for organizations

--- terraform/api_gateway_organizations.tf
+++ terraform/api_gateway_organizations.tf
@@ -1,30 +1,26 @@
 # Organizations API Gateway integration
+
+# /organizations resource
 resource "aws_api_gateway_resource" "organizations" {
   rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
   parent_id   = aws_api_gateway_rest_api.incident_cmd.root_resource_id
   path_part   = "organizations"
 }
 
+# /organizations/{org_id} resource
 resource "aws_api_gateway_resource" "organizations_id" {
   rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
   parent_id   = aws_api_gateway_resource.organizations.id
   path_part   = "{org_id}"
 }
 
+# GET /organizations
 resource "aws_api_gateway_method" "organizations_root" {
   rest_api_id   = aws_api_gateway_rest_api.incident_cmd.id
   resource_id   = aws_api_gateway_resource.organizations.id
   http_method   = "ANY"
   authorization = "NONE"
 }
-
-resource "aws_api_gateway_method" "organizations_id" {
-  rest_api_id   = aws_api_gateway_rest_api.incident_cmd.id
-  resource_id   = aws_api_gateway_resource.organizations_id.id
-  http_method   = "ANY"
-  authorization = "NONE"
-}
-
 resource "aws_api_gateway_integration" "organizations_root" {
   rest_api_id             = aws_api_gateway_rest_api.incident_cmd.id
   resource_id             = aws_api_gateway_resource.organizations.id
@@ -33,7 +29,21 @@ resource "aws_api_gateway_integration" "organizations_root" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.organizations.invoke_arn
 }
+resource "aws_lambda_permission" "apigw_organizations" {
+  statement_id  = "AllowAPIGatewayInvokeOrganizations"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.organizations.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.incident_cmd.execution_arn}/*/*/organizations*"
+}
 
+# GET /organizations/{org_id}
+resource "aws_api_gateway_method" "organizations_id" {
+  rest_api_id   = aws_api_gateway_rest_api.incident_cmd.id
+  resource_id   = aws_api_gateway_resource.organizations_id.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
 resource "aws_api_gateway_integration" "organizations_id" {
   rest_api_id             = aws_api_gateway_rest_api.incident_cmd.id
   resource_id             = aws_api_gateway_resource.organizations_id.id
@@ -43,13 +53,6 @@ resource "aws_api_gateway_integration" "organizations_id" {
   uri                     = aws_lambda_function.organizations.invoke_arn
 }
 
-resource "aws_lambda_permission" "apigw_organizations" {
-  statement_id  = "AllowAPIGatewayInvokeOrganizations"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.organizations.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.incident_cmd.execution_arn}/*/*/organizations*"
-}
 
 # CORS OPTIONS for /organizations
 resource "aws_api_gateway_method" "organizations_options" {
@@ -58,16 +61,16 @@ resource "aws_api_gateway_method" "organizations_options" {
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
-
 resource "aws_api_gateway_integration" "organizations_options" {
-  rest_api_id             = aws_api_gateway_rest_api.incident_cmd.id
-  resource_id             = aws_api_gateway_resource.organizations.id
-  http_method             = aws_api_gateway_method.organizations_options.http_method
-  type                    = "MOCK"
-  request_templates       = { "application/json" = "{\"statusCode\": 200}" }
-  integration_http_method = "OPTIONS"
+  depends_on  = [aws_api_gateway_method.organizations_options]
+  rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
+  resource_id = aws_api_gateway_resource.organizations.id
+  http_method = aws_api_gateway_method.organizations_options.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
 }
-
 resource "aws_api_gateway_method_response" "organizations_options" {
   rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
   resource_id = aws_api_gateway_resource.organizations.id
@@ -79,19 +82,18 @@ resource "aws_api_gateway_method_response" "organizations_options" {
     "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
-
 resource "aws_api_gateway_integration_response" "organizations_options" {
   rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
   resource_id = aws_api_gateway_resource.organizations.id
   http_method = aws_api_gateway_method.organizations_options.http_method
   status_code = aws_api_gateway_method_response.organizations_options.status_code
+  response_templates = {
+    "application/json" = ""
+  }
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-  response_templates = {
-    "application/json" = ""
   }
 }
 
@@ -102,16 +104,16 @@ resource "aws_api_gateway_method" "organizations_id_options" {
   http_method   = "OPTIONS"
   authorization = "NONE"
 }
-
 resource "aws_api_gateway_integration" "organizations_id_options" {
-  rest_api_id             = aws_api_gateway_rest_api.incident_cmd.id
-  resource_id             = aws_api_gateway_resource.organizations_id.id
-  http_method             = aws_api_gateway_method.organizations_id_options.http_method
-  type                    = "MOCK"
-  request_templates       = { "application/json" = "{\"statusCode\": 200}" }
-  integration_http_method = "OPTIONS"
+  depends_on  = [aws_api_gateway_method.organizations_id_options]
+  rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
+  resource_id = aws_api_gateway_resource.organizations_id.id
+  http_method = aws_api_gateway_method.organizations_id_options.http_method
+  type        = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
 }
-
 resource "aws_api_gateway_method_response" "organizations_id_options" {
   rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
   resource_id = aws_api_gateway_resource.organizations_id.id
@@ -123,18 +125,18 @@ resource "aws_api_gateway_method_response" "organizations_id_options" {
     "method.response.header.Access-Control-Allow-Origin"  = true
   }
 }
-
 resource "aws_api_gateway_integration_response" "organizations_id_options" {
+  depends_on  = [aws_api_gateway_integration.organizations_id_options]
   rest_api_id = aws_api_gateway_rest_api.incident_cmd.id
   resource_id = aws_api_gateway_resource.organizations_id.id
   http_method = aws_api_gateway_method.organizations_id_options.http_method
   status_code = aws_api_gateway_method_response.organizations_id_options.status_code
+  response_templates = {
+    "application/json" = ""
+  }
   response_parameters = {
     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
     "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
-  response_templates = {
-    "application/json" = ""
   }
 }
