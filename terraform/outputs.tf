d087f6682b0b2534bc289b585bc1fdc3abab23a2
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-28 06:58:43 -0400

    refactor: Remove obsolete LaunchDarkly feature flag outputs for super admin and admin access

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,21 +10,6 @@ output "launchdarkly_environment_client_side_id" {
   sensitive   = true
 }
 
-output "launchdarkly_super_admin_access_flag_key" {
-  description = "Key for the LaunchDarkly super-admin-access feature flag."
-  value       = launchdarkly_feature_flag.super_admin_access.key
-}
-
-output "launchdarkly_admin_access_flag_key" {
-  description = "Key for the LaunchDarkly admin-access feature flag."
-  value       = launchdarkly_feature_flag.admin_access.key
-}
-
-output "launchdarkly_show_radio_resources_flag_key" {
-  description = "Key for the LaunchDarkly show-radio-resources feature flag."
-  value       = launchdarkly_feature_flag.show_radio_resources.key
-}
-
 output "api_url" {
   description = "Base URL for the deployed API using the custom domain."
   value       = "https://${aws_api_gateway_domain_name.custom.domain_name}"
