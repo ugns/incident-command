13c7e2c489f9d20820b48085a85a8104a64e7b1c
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-28 02:56:21 -0400

    feat: Add output for LaunchDarkly show-radio-resources feature flag

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,6 +15,11 @@ output "launchdarkly_admin_access_flag_key" {
   value       = launchdarkly_feature_flag.admin_access.key
 }
 
+output "launchdarkly_show_radio_resources_flag_key" {
+  description = "Key for the LaunchDarkly show-radio-resources feature flag."
+  value       = launchdarkly_feature_flag.show_radio_resources.key
+}
+
 output "api_url" {
   description = "Base URL for the deployed API using the custom domain."
   value       = "https://${aws_api_gateway_domain_name.custom.domain_name}"
