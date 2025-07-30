4e4123f0c97088078f48f08861f997a8e4cf305d
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-07-30 17:09:53 -0400

    fix: Update DynamoDB table configuration to use orgId as hash key and connectionId as range key
    Update DynamoDB table keys for improved data organization
    
    Changes the hash key to `orgId` and adds `connectionId` as a range key for better query performance.
    
    Removes the `userId` attribute definition, streamlining the table structure for WebSocket connections.

--- terraform/ws_api_dynamodb.tf
+++ terraform/ws_api_dynamodb.tf
@@ -2,19 +2,20 @@
 resource "aws_dynamodb_table" "ws_connections" {
   name         = "WebSocketConnections"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "connectionId"
+  hash_key     = "orgId"
+  range_key    = "connectionId"
 
-  attribute {
-    name = "connectionId"
-    type = "S"
-  }
   attribute {
     name = "orgId"
     type = "S"
   }
   attribute {
-    name = "userId"
+    name = "connectionId"
     type = "S"
   }
+  # attribute {
+  #   name = "userId"
+  #   type = "S"
+  # }
   # Add more attributes/indexes as needed for queries
 }
