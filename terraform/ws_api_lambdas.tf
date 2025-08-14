b0123cfce91b238f54fbc4927f06ed49bf6b717f
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-08-14 15:21:38 -0400

    feat: Update Lambda function configurations to use specific handler files and remove unused archive_file resources

--- terraform/ws_api_lambdas.tf
+++ terraform/ws_api_lambdas.tf
@@ -92,18 +92,18 @@ resource "aws_iam_role_policy" "ws_lambda_dynamodb_policy" {
 
 # Lambda functions for WebSocket API
 
-data "archive_file" "ws_connect" {
-  type        = "zip"
-  source_dir  = "../lambda/ws_connect"
-  output_path = "../lambda/ws_connect.zip"
-}
+# data "archive_file" "ws_connect" {
+#   type        = "zip"
+#   source_dir  = "../lambda/ws_connect"
+#   output_path = "../lambda/ws_connect.zip"
+# }
 resource "aws_lambda_function" "ws_connect" {
   function_name    = "EventCoord-ws_connect_handler"
-  filename         = data.archive_file.ws_connect.output_path
+  filename         = "../lambda/ws_connect/handler.py"
   handler          = "handler.lambda_handler"
   runtime          = var.lambda_runtime
   role             = aws_iam_role.lambda_exec.arn
-  source_code_hash = data.archive_file.ws_connect.output_base64sha256
+  source_code_hash = filebase64sha256("../lambda/ws_connect/handler.py")
   timeout          = 10
   layers           = [aws_lambda_layer_version.shared.arn]
   environment {
@@ -118,18 +118,18 @@ resource "aws_lambda_function" "ws_connect" {
   }
 }
 
-data "archive_file" "ws_disconnect" {
-  type        = "zip"
-  source_dir  = "../lambda/ws_disconnect"
-  output_path = "../lambda/ws_disconnect.zip"
-}
+# data "archive_file" "ws_disconnect" {
+#   type        = "zip"
+#   source_dir  = "../lambda/ws_disconnect"
+#   output_path = "../lambda/ws_disconnect.zip"
+# }
 resource "aws_lambda_function" "ws_disconnect" {
   function_name    = "EventCoord-ws_disconnect_handler"
-  filename         = data.archive_file.ws_disconnect.output_path
+  filename         = "../lambda/ws_disconnect/handler.py"
   handler          = "handler.lambda_handler"
   runtime          = var.lambda_runtime
   role             = aws_iam_role.lambda_exec.arn
-  source_code_hash = data.archive_file.ws_disconnect.output_base64sha256
+  source_code_hash = filebase64sha256("../lambda/ws_disconnect/handler.py")
   timeout          = 10
   layers           = [aws_lambda_layer_version.shared.arn]
   environment {
@@ -143,18 +143,18 @@ resource "aws_lambda_function" "ws_disconnect" {
   }
 }
 
-data "archive_file" "ws_default" {
-  type        = "zip"
-  source_dir  = "../lambda/ws_default"
-  output_path = "../lambda/ws_default.zip"
-}
+# data "archive_file" "ws_default" {
+#   type        = "zip"
+#   source_dir  = "../lambda/ws_default"
+#   output_path = "../lambda/ws_default.zip"
+# }
 resource "aws_lambda_function" "ws_default" {
   function_name    = "EventCoord-ws_default_handler"
-  filename         = data.archive_file.ws_default.output_path
+  filename         = "../lambda/ws_default/handler.py"
   handler          = "handler.lambda_handler"
   runtime          = var.lambda_runtime
   role             = aws_iam_role.lambda_exec.arn
-  source_code_hash = data.archive_file.ws_default.output_base64sha256
+  source_code_hash = filebase64sha256("../lambda/ws_default/handler.py")
   timeout          = 10
   layers           = [aws_lambda_layer_version.shared.arn]
   environment {
@@ -169,11 +169,11 @@ resource "aws_lambda_function" "ws_default" {
 }
 
 # Archive and Lambda for notify_ws_stream (DynamoDB Streams handler)
-data "archive_file" "notify_ws_stream" {
-  type        = "zip"
-  source_dir  = "../lambda/notify_ws_stream"
-  output_path = "../lambda/notify_ws_stream.zip"
-}
+  # data "archive_file" "notify_ws_stream" {
+  #   type        = "zip"
+  #   source_dir  = "../lambda/notify_ws_stream"
+  #   output_path = "../lambda/notify_ws_stream.zip"
+  # }
 
 resource "aws_lambda_function" "notify_ws_stream" {
   function_name    = "EventCoord-notify_ws_stream_handler"
@@ -181,7 +181,7 @@ resource "aws_lambda_function" "notify_ws_stream" {
   handler          = "handler.lambda_handler"
   runtime          = var.lambda_runtime
   role             = aws_iam_role.lambda_exec.arn
-  source_code_hash = data.archive_file.notify_ws_stream.output_base64sha256
+  source_code_hash = filebase64sha256("../lambda/notify_ws_stream/handler.py")
   timeout          = 10
   layers           = [aws_lambda_layer_version.shared.arn]
   environment {
