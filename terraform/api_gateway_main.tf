e33cc34c5220c814826fb2bfd4d229dccf5033a4
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-08-15 17:59:01 -0400

    fix: Start debugging by going basic

--- terraform/api_gateway_main.tf
+++ terraform/api_gateway_main.tf
@@ -2,7 +2,7 @@ module "api" {
   source  = "cloudposse/api-gateway/aws"
   version = "0.9.0"
 
-  openapi_config       = templatefile("${path.module}/openapi.tftpl", merge({}, local.lambda_invoke_arn_map))
+  openapi_config       = yamldecode(templatefile("${path.module}/openapi.tftpl", merge({}, local.lambda_invoke_arn_map)))
   stage_name           = var.stage_name
   xray_tracing_enabled = true
   endpoint_type        = "REGIONAL"
