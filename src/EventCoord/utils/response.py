6ed7e55e94ce51fca0de3e76772e9a733517024d
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2025-08-18 12:15:58 -0400

    feat: Add JWT claims decoding functionality to handle API Gateway events

--- src/EventCoord/utils/response.py
+++ src/EventCoord/utils/response.py
@@ -1,6 +1,8 @@
 import json
+from authlib.jose import jwt, JWTClaims
 from aws_lambda_typing.responses import APIGatewayProxyResponseV2
-
+from aws_lambda_typing.events import APIGatewayProxyEventV2
+from typing import Optional
 
 def build_response(
     status_code: int,
@@ -12,3 +14,21 @@ def build_response(
         'headers': headers or {},
         'body': json.dumps(body)
     }
+
+def decode_claims(
+    event: APIGatewayProxyEventV2
+) -> Optional[JWTClaims]:
+    """
+    Decode API Gateway Proxy Event to collect JWT and return claims
+    """
+    token = None
+    if 'headers' in event and (event['headers'].get('authorization') or event['headers'].get('Authorization')):
+        token = event['headers'].get(
+            'authorization') or event['headers'].get('Authorization')
+        if token and token.startswith('Bearer '):
+            token = token.replace("Bearer ", "").strip()
+    if token:
+        # Use empty string as key for unsigned JWTs and pass claims_cls and claims_options
+        payload = jwt.decode(token, '', claims_cls=JWTClaims, claims_options={"verify_signature": False})
+        return payload
+    return None
